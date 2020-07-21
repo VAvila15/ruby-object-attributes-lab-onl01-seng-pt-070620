3eb576818aa495681b032e975ca75a7831854a88
Victor Avila <avilavictor1570@gmail.com>
2020-07-21 03:03:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,17 @@
 class Person
-  def person=()
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job(peron_job)
+    @job = (peron_job)
+  end
+  
+  def job
+    @job
+  end
+end
